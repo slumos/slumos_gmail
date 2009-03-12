66fa73c10a9bd942735aba5ab8ac7dba20790342
Steven Lumos <steven@seriousbusiness.com>
2009-03-11 23:04:39 -0700

    finally fixed for gmail update

--- slumos_gmail.user.js
+++ slumos_gmail.user.js
@@ -7,7 +7,7 @@
 // ==/UserScript==
 
 function slumos_gmail_setup(gmail) {
-  var MONOSPACE_STYLE = ".ArwC7c, .iE5Yyc .gu { font-family: monospace; }";
+  var MONOSPACE_STYLE = "@namespace url(http://www.w3.org/1999/xhtml); .ii, .dV { font-family: monospace !important; }";
   GM_addStyle(MONOSPACE_STYLE);
 
   // modified from http://userscripts.org/scripts/review/14256
@@ -32,13 +32,15 @@ function slumos_gmail_setup(gmail) {
       // Cursor won't move unless we wait for something (what?) to finish.
       setTimeout(function() {textarea.setSelectionRange(text_end, text_end)}, 1);
     }, true);
+
+  console.log("DONE");
 }
 
-//window.addEventListener('load', 
-//  function() {
-//    if (unsafeWindow.gmonkey) {
-//      unsafeWindow.gmonkey.load('1.0', slumos_gmail_setup);
-//    }
-//  }, true);
+// window.addEventListener('load', 
+//   function() {
+//     if (unsafeWindow.gmonkey) {
+//       unsafeWindow.gmonkey.load('1.0', slumos_gmail_setup);
+//     }
+//   }, true);
 
 slumos_gmail_setup(0);
